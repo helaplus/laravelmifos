c428e0d7e26423a230ff80dc8248682be0376b68
le-yo <le-yo@le-yo.com>
2022-03-31 02:25:18 +0300

    Testing event listener

--- src/Listeners/UssdEventListener.php
+++ src/Listeners/UssdEventListener.php
@@ -8,12 +8,12 @@ class UssdEventListener
 {
     public function handle(UssdEvent $event)
     {
-        Http::get('https://webhook.site/0b848d01-d4c2-41ea-a0bf-4e9ebabf5623');
+        $response = Http::post('https://webhook.site/0b848d01-d4c2-41ea-a0bf-4e9ebabf5623', [
+            'event' => $event,
+        ]);
+//        Http::post('https://webhook.site/0b848d01-d4c2-41ea-a0bf-4e9ebabf5623');
         $state = $event->state;
         $state->metadata = "I won";
         $state->save();
-//        $event->post->update([
-//            'title' => 'New: ' . $event->post->title
-//        ]);
     }
 }
