62e2b618e83e16c5d9918a8e5a5371d4e9700b55
le-yo <le-yo@le-yo.com>
2022-03-31 15:15:59 +0300

    Giving it a try

--- src/Http/MifosHelperController.php
+++ src/Http/MifosHelperController.php
@@ -9,17 +9,14 @@ use Illuminate\Support\Facades\Http;
 class MifosHelperController extends Controller {
 
 
-    public static function MifosPostTransaction($endpoint,$data){
-        $url = "https://webhook.site/0b848d01-d4c2-41ea-a0bf-4e9ebabf5623";
-
+    public static function MifosPostTransaction($endpoint,$data,$options=""){
+        $url = config('laravelmifos.mifos_url') . "fineract-provider/api/v1/".$endpoint."?".$options."&tenantIdentifier=" .config('laravelmifos.mifos_tenant');
         return Http::withHeaders(
             [
                 'Content-Type' => 'application/json',
                 'Content-Length' => strlen($data)
             ]
         )->withBasicAuth(config('laravelmifos.mifos_username'),config('laravelmifos.mifos_password'))->post($url,$data);
-
-
     }
 
 }
