82ebd57888e09db5d7ba526fb9b540238ac49913
le-yo <le-yo@le-yo.com>
2022-03-31 15:26:55 +0300

    Troubleshooting curl

--- src/Http/MifosHelperController.php
+++ src/Http/MifosHelperController.php
@@ -10,16 +10,41 @@ class MifosHelperController extends Controller {
 
 
     public static function MifosPostTransaction($endpoint,$data,$options=""){
-        if(strlen($options)>0){ 
+        if(strlen($options)>0){
             $options = "&".$options;
         }
         $url = config('laravelmifos.mifos_url') . "fineract-provider/api/v1/".$endpoint."?tenantIdentifier=" .config('laravelmifos.mifos_tenant').$options;
-        return Http::withHeaders(
-            [
-                'Content-Type' => 'application/json',
-                'Content-Length' => strlen($data)
-            ]
-        )->withBasicAuth(config('laravelmifos.mifos_username'),config('laravelmifos.mifos_password'))->post($url,$data);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data))
+        );
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+        curl_setopt($ch, CURLOPT_USERPWD, config('laravelmifos.mifos_username').':'.config('laravelmifos.mifos_password'));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        $data = curl_exec($ch);
+        if ($errno = curl_errno($ch)) {
+            $error_message = curl_strerror($errno);
+            echo "cURL error ({$errno}):\n {$error_message}";
+        }
+        curl_close($ch);
+
+        $response = json_decode($data);
+
+        return $response;
+
+        //        return Http::withHeaders(
+//            [
+//                'Content-Type' => 'application/json',
+//                'Content-Length' => strlen($data)
+//            ]
+//        )->withBasicAuth(config('laravelmifos.mifos_username'),config('laravelmifos.mifos_password'))->post($url,$data);
     }
 
 }
