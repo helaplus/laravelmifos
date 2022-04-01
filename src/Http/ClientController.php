a91db0ff826c39b784fa469a0869b8b4be090982
le-yo <le-yo@le-yo.com>
2022-04-01 12:03:27 +0300

    Added methods for getClientByID getClientByExternalId and getClientByPhone

--- src/Http/ClientController.php
+++ src/Http/ClientController.php
@@ -13,4 +13,44 @@ class ClientController extends Controller {
         return MifosHelperController::MifosPostTransaction($endpoint, $data);
     }
 
+    public static function getClientByPhone($phone){
+        $user = FALSE;
+        $endpoint = 'search';
+        $options = "exactMatch=false&query=" . $phone . "&resource=clients";
+        // Get client
+        $post_data = [];
+        $client = MifosHelperController::MifosGetTransaction($endpoint, $post_data, $options);
+        if(isset($client[0])){
+            return  self::getClientByClientId($client[0]->entityId);
+        }else{
+            $no = substr($phone,-9);
+            $endpoint = 'search';
+            $options = "exactMatch=false&query=" . $no . "&resource=clients";
+            // Get client
+            $client = self::MifosGetTransaction($endpoint, $post_data = '',$options);
+            if(isset($client[0])){
+                return  self::getClientByClientId($client[0]->entityId);
+            }
+        }
+        return $user;
+    }
+
+    public static function getClientByExternalId($externalid){
+
+        $user = FALSE;
+        $endpoint = 'search';
+        $options = "exactMatch=true&query=" . $externalid . "&resource=clients,clientIdentifiers";
+        // Get client
+        $client = MifosHelperController::MifosGetTransaction($endpoint, $post_data = [],$options);
+        return $client;
+    }
+
+
+    public static function getClientByClientId($client_id)
+    {
+        $endpoint = 'clients/'.$client_id;
+        $client = MifosHelperController::MifosGetTransaction($endpoint,null);
+        return $client;
+    }
+
 }
