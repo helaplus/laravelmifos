0ad5730856c7122d08194b4eae1bee581726d893
le-yo <le-yo@le-yo.com>
2022-04-01 14:12:03 +0300

    Updated Loan controller

--- src/Http/LoanController.php
+++ src/Http/LoanController.php
@@ -1,12 +1,13 @@
 <?php
 
+
 namespace Helaplus\Laravelmifos\Http;
 
 use Helaplus\Laravelmifos\Http\MifosHelperController;
 
-class ClientController extends Controller {
+class LoanController extends Controller {
     public static function getLoanProduct($product_id){
-        $endpoint = "loanproducts/".$product_id;
+        $endpoint = "loanproducts/".$product_id; 
         $options = "template=true";
         return MifosHelperController::MifosGetTransaction($endpoint,$options);
     }
