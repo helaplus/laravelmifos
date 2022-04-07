98e5bec9aaf1363dc3cb28ca25ea8dbf9cdb62b8
le-yo <le-yo@le-yo.com>
2022-04-07 03:30:24 +0300

    Added array of loans to getLoanBalance method

--- src/Http/LoanController.php
+++ src/Http/LoanController.php
@@ -38,10 +38,12 @@ class LoanController extends Controller {
 
     public static function getLoanBalance($client_id){
         $loanAccounts = ClientController::getClientLoanAccounts($client_id);
-        $balance = 0;
-        foreach ($loanAccounts as $lA){
+        $balance = [];
+        $balance['loans'] = [];
+        foreach ($loanAccounts as $lA){ 
             if($lA->status->id ==300){
-                $balance = $balance+$lA->loanBalance;
+                $balance['amount'] = $balance['amount']+$lA->loanBalance;
+                array_push($balance['loans'],$lA);
             }
         }
     }
