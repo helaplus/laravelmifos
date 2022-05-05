b38d8f0d899438d18e5ce85012a10443686f70ba
le-yo <le-yo@le-yo.com>
2022-05-05 14:57:33 +0300

    Added /mpesa/stkReceiver

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use Helaplus\Laravelmifos\Http\MpesaController;
 
-Route::post('/mpesa/stkReceiver', [MpesaController::class, 'stkReceiver'])->name('mpesa.stkReceiver');  
+Route::post('/mpesa/stkReceiver', [MpesaController::class, 'stkReceiver'])->name('mpesa.stkReceiver');
 Route::post('/mpesa/c2bReceiver', [MpesaController::class, 'c2bReceiver'])->name('mpesa.c2bReceiver');
